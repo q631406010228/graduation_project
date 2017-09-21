e2bfda1490daafefa8ef764e31325d42bf6b891c
smolefeng <32058053+smolefeng@users.noreply.github.com>
2017-09-21 17:31:29 +0800

    添加链接数据库工具；添加学生、教职工模型

---
+++ src/main/java/com/zr/model/Employee.java
@@ -0,0 +1,48 @@
+package com.zr.model;
+
+public class Employee {
+	private int eid; //职工id
+	private String ename; //职工姓名
+	private int ecol; //学院id
+	private int replyid; //答辩id
+	private int emnum; //教职工号
+	private String epsw; //职工密码
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getEcol() {
+		return ecol;
+	}
+	public void setEcol(int ecol) {
+		this.ecol = ecol;
+	}
+	public int getReplyid() {
+		return replyid;
+	}
+	public void setReplyid(int replyid) {
+		this.replyid = replyid;
+	}
+	public int getEmnum() {
+		return emnum;
+	}
+	public void setEmnum(int emnum) {
+		this.emnum = emnum;
+	}
+	public String getEpsw() {
+		return epsw;
+	}
+	public void setEpsw(String epsw) {
+		this.epsw = epsw;
+	}
+	
+	
+}
