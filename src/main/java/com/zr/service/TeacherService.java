af3012990ab5eef349e9267fb2c609fa6d74a7d7
fwzcy <32058172+fwzcy@users.noreply.github.com>
2017-09-27 14:17:25 +0800

    论文上传，成绩修改

--- src/main/java/com/zr/service/TeacherService.java
+++ src/main/java/com/zr/service/TeacherService.java
@@ -3,5 +3,22 @@ package com.zr.service;
 import net.sf.json.JSONObject;
 
 public interface TeacherService {
+	/**
+	 * 获取学生所有论文
+	 * @return json数据对象
+	 */
 	public JSONObject getPapers();
+	/**
+	 * 获取学生答辩成绩
+	 * @return json数据对象
+	 */
+    public JSONObject getScoresOfStu(int eid);
+    
+   /**
+    * 添加学生成绩    
+    * @param snum 学生学号
+    * @param sname 学生姓名
+    * @param score  成绩
+    */
+    public void alterStudentScore(int sid ,int score);
 }
