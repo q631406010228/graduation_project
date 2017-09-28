2c33ecab74dea76982f0e8ee5afae7afcc9565b5
耗空 <601480267@qq.com>
2017-09-28 11:09:55 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/q631406010228/graduation_project""
    This reverts commit 6cc0a38a8bffc3aef4193eea7d95ef6f47dad0f7.

--- src/main/java/com/zr/dao/StaffDao.java
+++ src/main/java/com/zr/dao/StaffDao.java
@@ -1,6 +1,9 @@
 package com.zr.dao;
 
+import java.util.List;
+
 import com.zr.model.Staff;
+import com.zr.model.Subject;
 
 /** 
 * @author SkySK: 
@@ -9,4 +12,14 @@ import com.zr.model.Staff;
 */
 public interface StaffDao {
 	public Staff getStaff(String ename, String epsw);
+	
+	public int getSubjectCount();
+
+	public List<Subject> getSubjects(int i, int pageSize);
+	
+	public void agreeSubject(int sub_id);
+
+	public void returnSubject(int sub_id);
+
+	public void deleteSubject(int sub_id);
 }
---
+++ src/main/java/com/zr/service/impl/ReplyServiceImpl.java
@@ -0,0 +1,36 @@
+package com.zr.service.impl;
+
+import com.zr.dao.ReplyDao;
+import com.zr.dao.StaffDao;
+import com.zr.dao.impl.ReplyDaoImpl;
+import com.zr.dao.impl.StaffDaoImpl;
+import com.zr.service.ReplyService;
+
+import net.sf.json.JSONObject;
+
+public class ReplyServiceImpl implements ReplyService {
+	ReplyDao rdao=new ReplyDaoImpl();
+	@Override
+	public JSONObject getReplys(int page, int pageSize) {
+		JSONObject  j = new JSONObject();
+		j.put("total", rdao.getReplyCount());
+		j.put("rows", rdao.getReplys((page-1)*pageSize, pageSize));
+		return j;
+	}
+	@Override
+	public void addReply(String reply_site, int sub_id, String reply_start, String reply_end, int e_id) {
+		rdao.addReply(reply_site, sub_id, reply_start, reply_end, e_id);
+		
+	}
+	@Override
+	public void deleteReply(int reply_id) {
+		// TODO Auto-generated method stub
+		rdao.deleteReply(reply_id);
+	}
+	@Override
+	public void alertReply(int reply_id,String reply_site, int sub_id, String reply_start, String reply_end, int e_id) {
+		// TODO Auto-generated method stub
+		rdao.alertReply(reply_id,reply_site, sub_id, reply_start, reply_end, e_id);
+	}
+
+}
